307cb23bfd7eb7d3a11d8086c559133c134796a4
LuciLua <luci.lua81@gmail.com>
2022-06-27 12:20:58 -0300

    💓 setTimeOut heart error when not put nothing in some input

--- app.js
+++ app.js
@@ -7,12 +7,24 @@ function combine() {
     const combinator = {
         random: (Math.random() * 100).toFixed(0),
         match(you = "_____", crush = "____") {
-            return `<div class="hightlight">${you}</div><span id="love"> ${this.random}%💓</span> <div class="hightlight">${crush}</div>`
+            return `
+            <div class="result">
+                <div class="hightlight">${you}</div>
+                    <span id="love"> ${this.random}%💓</span>
+                <div class="hightlight">${crush}</div>
+            </div>`
         }
     }
 
+    const porUmTempo = () => {
+        match.innerHTML = `<div class="notPut">Coloque seu nome e o/a do/a seu/sua crush`
+        setTimeout( () => {
+            match.innerHTML = '💓'
+        }, 2000)
+    }
+
     you == "" || crush == ""
-        ? match.textContent = "Coloque seu nome e o/a do/a seu/sua crush"
+        ? porUmTempo()
         : match.innerHTML = combinator.match(you, crush)
 }
 
